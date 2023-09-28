a20709613d0d4f78ae4aee9f2c66098a37aa26c2
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2023-09-28 20:18:41 +0600

    vowel or consonent

--- 08_vowel_or_consonant.c
+++ 08_vowel_or_consonant.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+int main()
+{
 
-int main(){
+    char c;
 
+    printf("Enter a character: ");
+    scanf("%c", &c);
 
-return 0;
+    c = tolower(c);
+
+    if (c >= 'a' && c <= 'z')
+    {
+
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+        {
+            printf("Vowel");
+        }
+        else
+        {
+            printf("Consonent");
+        }
+    }
+    else
+    {
+        printf("Invalid character");
+    }
+
+    return 0;
 }
