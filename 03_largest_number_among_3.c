633667344d91536f9f57a12780e565c89a2e172d
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2023-09-28 12:45:24 +0600

    largest number

---
+++ 03_largest_number_among_3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+int a, b, c;
+
+printf("Enter value for a: ");
+scanf("%d", &a);
+
+printf("Enter value for b: ");
+scanf("%d", &b);
+
+printf("Enter value for c: ");
+scanf("%d", &c);
+
+if (a>b && a>c){
+printf("a (%d) is the largest number", a);
+} else if (b>a && b>c){
+printf("b (%d) is the largest number", b);
+} else {
+printf("c (%d) is the largest number", c);
+}
+
+
+return 0;
+
+
+}
