20130d369b9e7c4cca4da93863f5e1711699ac9e
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2023-09-28 21:32:37 +0600

    sum of digit

--- 09_sum_of_digits_of_a_number.c
+++ 09_sum_of_digits_of_a_number.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
+    int n, sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
 
-return 0;
+    while (n != 0)
+    {
+
+        int r = n % 10;
+        sum += r;
+        n /= 10;
+    }
+
+    printf("%d", sum);
+
+    return 0;
 }
