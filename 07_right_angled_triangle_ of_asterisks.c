0ec0eb2a8c07337ffb27ba97c5ed48cb124e222e
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2023-09-28 17:53:16 +0600

    asterisk

--- 07_right_angled_triangle_ of_asterisks.c
+++ 07_right_angled_triangle_ of_asterisks.c
@@ -2,6 +2,19 @@
 
 int main(){
 
+int n;
+
+printf("Enter a number: ");
+scanf("%d", &n);
+
+for (int i=1; i<=n; i++){
+for (int j=1; j<=i; j++){
+printf("*");
+}
+printf("\n");
+}
+
+
 
 return 0;
 }
