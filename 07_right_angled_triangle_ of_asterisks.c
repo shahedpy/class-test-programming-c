dc2be92eafcf44efd883b03e988190437ec55b29
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2023-09-28 18:00:06 +0600

    reformat

--- 07_right_angled_triangle_ of_asterisks.c
+++ 07_right_angled_triangle_ of_asterisks.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
-int n;
+    int n;
 
-printf("Enter a number: ");
-scanf("%d", &n);
+    printf("Enter a number: ");
+    scanf("%d", &n);
 
-for (int i=1; i<=n; i++){
-for (int j=1; j<=i; j++){
-printf("*");
-}
-printf("\n");
-}
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
 
-
-
-return 0;
+    return 0;
 }
