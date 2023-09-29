b697c4d3d0ab0217769baef1f988b1c47b26a912
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2023-09-29 12:39:28 +0600

    reverse number

--- 13_reverse_number.c
+++ 13_reverse_number.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
 
+    while (n != 0)
+    {
+        int r = n % 10;
+        n /= 10;
+        printf("%d", r);
+    }
 
-return 0;
+    return 0;
 }
