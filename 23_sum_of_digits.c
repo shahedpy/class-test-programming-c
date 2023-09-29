c36d48c550c0631bf26b458fff3b949b6c298d2d
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2023-09-29 20:03:04 +0600

    sum of digits

--- 23_sum_of_digits.c
+++ 23_sum_of_digits.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
+    int number, digit, sum = 0;
 
+    printf("Enter a number: ");
+    scanf("%d", &number);
 
-return 0;
+    while (number != 0)
+    {
+        digit = number % 10;
+        sum += digit;
+        number /= 10;
+    }
+
+    printf("The sum of the digits is: %d\n", sum);
+
+    return 0;
 }
