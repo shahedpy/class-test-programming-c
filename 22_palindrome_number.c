8f590d614b6aaae3bfdd39d53df8d0c591ea7fc6
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2023-09-29 19:29:58 +0600

    palindrome

--- 22_palindrome_number.c
+++ 22_palindrome_number.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
-int main(){
+int main() {
+    int num, reversedNum = 0, originalNum, remainder;
 
+    printf("Enter an integer: ");
+    scanf("%d", &num);
 
-return 0;
+    originalNum = num;
+
+    while (num != 0) {
+        remainder = num % 10;
+        reversedNum = reversedNum * 10 + remainder;
+        num /= 10;
+    }
+
+    if (originalNum == reversedNum) {
+        printf("%d is a palindrome.\n", originalNum);
+    } else {
+        printf("%d is not a palindrome.\n", originalNum);
+    }
+
+    return 0;
 }
