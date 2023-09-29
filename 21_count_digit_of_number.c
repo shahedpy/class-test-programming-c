2af9ec7bc4289d046373356ad2d88e210d9d8048
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2023-09-29 19:13:09 +0600

    count of digit

--- 21_count_digit_of_number.c
+++ 21_count_digit_of_number.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
 int main(){
+    int n, count;
+    printf("Enter a number: ");
+    scanf("%d", &n);
 
+    while (n != 0)
+    {
+        n/=10;
+        count++;
+    }
+
+    printf("%d", count);
+    
 
 return 0;
 }
