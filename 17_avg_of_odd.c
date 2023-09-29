75707a7185ba26a97588daa245cb90e56655c852
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2023-09-29 18:50:49 +0600

    avg of odd

--- 17_avg_of_odd.c
+++ 17_avg_of_odd.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
-int main(){
+int main() {
+    int n;
+    printf("Enter a positive integer n: ");
+    scanf("%d", &n);
 
+    int sum = 0;
+    int count = 0;
 
-return 0;
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 != 0) {
+            sum += i;
+            count++;
+        }
+    }
+
+    if (count > 0) {
+        double average = (double)sum / count;
+        printf("Average of all odd numbers from 1 to %d is %.2lf\n", n, average);
+    }
+
+    return 0;
 }
