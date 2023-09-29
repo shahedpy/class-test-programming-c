e69455f91415a00cb25f33e058449debeedcc554
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2023-09-29 21:52:44 +0600

    lcm

--- 29_least_common_multiplication.c
+++ 29_least_common_multiplication.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
+    int num1, num2, max;
 
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Enter second number: ");
+    scanf("%d", &num2);
 
-return 0;
+    max = (num1 > num2) ? num1 : num2;
+
+    while (1)
+    {
+        if (max % num1 == 0 && max % num2 == 0)
+        {
+            printf("LCM of %d and %d is %d\n", num1, num2, max);
+            break;
+        }
+        max++;
+    }
+
+    return 0;
 }
