af64bc20a66df830ad010ab319a79281e001f9c9
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2023-09-29 20:13:31 +0600

    pattern printing

--- 24_generate_pattern.c
+++ 24_generate_pattern.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
+    int N;
+    printf("Enter the value of N: ");
+    scanf("%d", &N);
 
+    for (int i = 1; i <= N; i++)
+    {
 
-return 0;
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
