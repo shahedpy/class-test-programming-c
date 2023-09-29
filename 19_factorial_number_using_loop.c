b221f2a065aec456e38489489fa57bd127bc7bf6
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2023-09-29 19:04:40 +0600

    factorial

--- 19_factorial_number_using_loop.c
+++ 19_factorial_number_using_loop.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
-int main(){
+int main() {
+    int n;
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    int factorial = 1;
 
 
-return 0;
+    while (n > 1) {
+        factorial *= n;
+        n--;
+    }
+
+    printf("Factorial: %d", factorial);
+
+    return 0;
 }
