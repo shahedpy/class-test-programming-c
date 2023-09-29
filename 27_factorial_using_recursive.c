168b8e4551fa856afc8bf94135d887e6a1f3cec5
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2023-09-29 20:43:44 +0600

    recursive

--- 27_factorial_using_recursive.c
+++ 27_factorial_using_recursive.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
 
-// Function to calculate the factorial of a number using recursion
-unsigned long long factorial(int n) {
-    if (n == 0 || n == 1) {
-        return 1; // Base case: factorial of 0 and 1 is 1
-    } else {
-        return n * factorial(n - 1); // Recursive case: n! = n * (n-1)!
+unsigned long long factorial(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * factorial(n - 1);
     }
 }
 
-int main() {
+int main()
+{
     int num;
     printf("Enter a positive integer: ");
     scanf("%d", &num);
 
-    if (num < 0) {
+    if (num < 0)
+    {
         printf("Factorial is not defined for negative numbers.\n");
-    } else {
+    }
+    else
+    {
         unsigned long long result = factorial(num);
         printf("Factorial of %d = %llu\n", num, result);
     }
