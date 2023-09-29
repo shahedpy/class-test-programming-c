38e30ee60d4327340d4c230db4fcf0f05ca7ede1
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2023-09-29 20:26:19 +0600

    square series

--- 25_square_series.c
+++ 25_square_series.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
+    int N;
 
+    // Input the value of N
+    printf("Enter the value of N: ");
+    scanf("%d", &N);
 
-return 0;
+    // Print the series
+    printf("Series: ");
+    for (int i = 1; i <= N; i++)
+    {
+        if (i < N)
+        {
+            printf("%d² + ", i);
+        }
+        else
+        {
+            printf("%d²", i);
+        }
+    }
+    printf("\n");
+
+    return 0;
 }
