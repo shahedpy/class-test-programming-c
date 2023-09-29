5e106eca25d7f62995677e5c675b268973dfed08
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2023-09-29 18:55:03 +0600

    gcd

--- 18_greatest_common_divisor.c
+++ 18_greatest_common_divisor.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
-int main(){
+int main() {
+    int num1, num2;
 
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
 
-return 0;
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    int gcd;
+
+    for (int i = 1; i <= num1 && i <= num2; ++i) {
+        if (num1 % i == 0 && num2 % i == 0) {
+            gcd = i;
+        }
+    }
+
+    printf("The GCD of %d and %d is %d\n", num1, num2, gcd);
+
+    return 0;
 }
