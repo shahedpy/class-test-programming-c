8f6f841fa060732ec9284d48daccba0bc10a22ab
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2023-09-29 22:00:39 +0600

    pattern

--- 30_print_pattern.c
+++ 30_print_pattern.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
+    int N;
+    printf("Enter the value of N: ");
+    scanf("%d", &N);
 
+    for (int i = 1; i <= N; i++)
+    {
 
-return 0;
+        for (int j = 1; j <= N - i; j++)
+        {
+            printf(" ");
+        }
+
+        for (int k = 1; k <= i; k++)
+        {
+            printf("* ");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
 }
