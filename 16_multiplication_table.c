5b0f11ddb08483faa4be5e7a1f7e8c013f124f01
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2023-09-29 18:27:13 +0600

    multiplication table

--- 16_multiplication_table.c
+++ 16_multiplication_table.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main()
+{
+    int n, c;
+
+    printf("Enter value for n: ");
+    scanf("%d", &n);
+    printf("Enter Number: ");
+    scanf("%d", &c);
+
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%d X %d = %d\n", c, i, c * i);
+    }
+
+    return 0;
+}
