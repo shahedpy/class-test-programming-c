80edfdb8dfc37532e6601ee9fe60efb3671ffe1d
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2023-09-29 21:03:48 +0600

    sum of prime number

--- 28_calculate_prime_number.c
+++ 28_calculate_prime_number.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
+    int N, i, j, isPrime, sum = 0;
 
+    printf("Enter a positive integer N: ");
+    scanf("%d", &N);
 
-return 0;
+    for (i = 2; i <= N; i++)
+    {
+        isPrime = 1;
+        for (j = 2; j * j <= i; j++)
+        {
+            if (i % j == 0)
+            {
+                isPrime = 0;
+                break;
+            }
+        }
+        if (isPrime)
+        {
+            sum += i;
+        }
+    }
+
+    printf("The sum of all prime numbers between 1 and %d is: %d\n", N, sum);
+
+    return 0;
 }
